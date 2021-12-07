310f07b02e822efc4c669109eef49051d5c1af0e
Gaylubeoil <92313524+Gaylubeoil@users.noreply.github.com>
2021-12-07 18:41:20 +0200

    Add files via upload

---
+++ Week 9/task1.cpp
@@ -0,0 +1,59 @@
+﻿// Task1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <iostream>
+const int MATRIX_SIZE = 5;
+void DrawMatrix(int arr[][MATRIX_SIZE]) {
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            std::cout << "Input Arr[" << i << "][" << j << "]: ";
+            std::cin >> arr[i][j];
+        }
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            std::cout << arr[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+bool EqualDiagonalsOfSmallSquares(int arr[][MATRIX_SIZE]) {
+    int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+    int help = MATRIX_SIZE / 2; // 2
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            if (i < (MATRIX_SIZE / 2) && j == i)
+            {
+                sum1 += arr[i][j];
+            }
+            if (i < (MATRIX_SIZE / 2) && j == MATRIX_SIZE - help)
+            {
+                sum2 += arr[i][j];
+            }
+            if (i > MATRIX_SIZE / 2 && j == (i + MATRIX_SIZE / 2) - MATRIX_SIZE)
+            {
+                sum3 += arr[i][j];
+            }
+            if (i > MATRIX_SIZE / 2 && j == i)
+            {
+                sum4 += arr[i][j];
+            }
+        }
+        help -= 1;
+    }
+    return(sum1 == sum2 && sum2 == sum3 && sum3 == sum4) ? true : false;
+}
+int main()
+{
+    int arr[MATRIX_SIZE][MATRIX_SIZE];
+    DrawMatrix(arr);
+    std::cout << EqualDiagonalsOfSmallSquares(arr);
+    return 0;
+}
+
---
+++ Week 9/task2.cpp
@@ -0,0 +1,28 @@
+﻿// Task2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <iostream>
+const int sizea = 64, sizeb = 64;
+bool MatchingElements(char arra[sizea], char arrb[sizeb]) {
+    for (int i = 0; i < sizea; i++)
+    {
+        return(arra[i] + 49 == arrb[i]) ? true : false;
+    }
+}
+int main()
+{
+    char a[sizeb] = { '0', '1', '2', '3', '4', '5', '6','7','8', '9' };
+    char b[sizea] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+    for (int i = 0; i < sizea; i++)
+    {
+        std::cout << "Enter Array[" << i << "]: ";
+        std::cin >> a[i];
+    }
+    for (int i = 0; i < sizeb; i++)
+    {
+        std::cout << "Enter Array[" << i << "]: ";
+        std::cin >> b[i];
+    }
+    std::cout << std::endl;
+    std::cout << MatchingElements(a, b);
+    return 0;
+}
+
